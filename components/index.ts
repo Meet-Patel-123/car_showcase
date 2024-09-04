79fc203f836a84d3a24d06dade93f8156569fc36
MeetPatel012 <meet.patel@cognitivecoreai.com>
2024-09-04 18:05:24 +0530

    lauout park completed

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import SearchBar from "./searchbar";
+import CustomeFilter from "./customfilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomeFilter,
+  SearchManufacturer,
+};
